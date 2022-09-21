1b00601534b1466d59190a78e122c812452bba9f
Adrian Liebel <aliebel@gmail.com>
2022-09-21 04:25:11 -0400

    Adding Philosopher Item

--- src/app/philosophers/philosopher-list/philosopher-list.component.ts
+++ src/app/philosophers/philosopher-list/philosopher-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Philosopher } from '../philosopher.model';
 
 @Component({
   selector: 'app-philosopher-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./philosopher-list.component.css']
 })
 export class PhilosopherListComponent implements OnInit {
+
+  philosophers: Philosopher[] = [
+    new Philosopher('Thales', '649 BC', 'Miletus Ionia', '599 BC', 'Miletus Ionia')
+  ];
 
   constructor() { }
 
