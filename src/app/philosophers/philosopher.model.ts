2de8e1191782108649cfbcfefcd97d4a464218b8
Adrian Liebel <letricjet@gmail.com>
2023-10-04 21:14:47 -0400

    Added ReferenceLinks

--- src/app/philosophers/philosopher.model.ts
+++ src/app/philosophers/philosopher.model.ts
@@ -1,3 +1,5 @@
+import { Reference } from '../references/reference.model';
+
 export class Philosopher {
     public name: string;
     public born: string;
@@ -5,13 +7,15 @@ export class Philosopher {
     public died: string;
     public deathPlace;
     public imageUrl: string;
+    public referenceLinks: Reference[];
 
-    constructor(name: string, born: string, birthPlace: string, died: string, deathPlace: string, imageUrl: string) {
+    constructor(name: string, born: string, birthPlace: string, died: string, deathPlace: string, imageUrl: string, referenceLinks: Reference[]) {
         this.name = name;
         this.born = born;
         this.birthPlace = birthPlace;
         this.died = died;
         this.deathPlace = deathPlace;
         this.imageUrl = imageUrl;
+        this.referenceLinks = referenceLinks;
     }
 }
